d534425f391d04a4d45ab3d03fecc8552e825639
nikhil-rathour <rathournikhil042@gmail.com>
2025-07-04 23:24:39 +0530

    api route

--- backend/src/routes/ai.routes.js
+++ backend/src/routes/ai.routes.js
@@ -5,7 +5,7 @@ const router  =  express.Router()
 router.post("/get-review" , aiController.getReview )
 
 router.get("/get-review", (req, res) => {
-    res.send("Welcome to the AI Code Reviewer API Server")
+    res.send("Api route")
 })
 
 
